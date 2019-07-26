12d9eb062c4bfa74dde3f4e673efa0cfa75448a2
Mikolaj Lukasik <mlukasik@gmail.com>
2019-07-26 20:31:18 +0100

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mikolaj Lukasik on 25/07/2019.
+//  Copyright © 2019 Mikolaj Lukasik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
